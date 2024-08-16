e17d8ab85c244d41bc59615680432da491a4662e
Kent Ross <k@mad.cash>
2024-08-16 14:47:01 -0700

    evolve DateTime fuzzing

--- fuzz/common/src/lib.rs
+++ fuzz/common/src/lib.rs
@@ -43,30 +43,46 @@ static DATE_RE: LazyLock<Regex> = LazyLock::new(|| {
 
 pub fn test_parse_date(data: &[u8]) {
     // input must be text
-    let Ok(s) = from_utf8(data) else {
+    let Ok(original_text) = from_utf8(data) else {
         return;
     };
+
     // parse input as a datetime
-    let Ok(t) = bilrost_types::Timestamp::from_str(s) else {
+    let Ok(timestamp) = bilrost_types::Timestamp::from_str(original_text) else {
+        let chrono_parse = chrono::DateTime::parse_from_rfc3339(original_text);
+        assert!(
+            chrono_parse.is_err(),
+            "bilrost_types failed to parse time, but chrono does parse this time: {}",
+            original_text
+        );
         return;
     };
+
     // check that it matches our regex pattern
-    assert!(DATE_RE.is_match(s));
-    // round trip from string again
-    let s2 = format!("{t}");
-    assert_eq!(Ok(&t), s2.parse().as_ref());
-    // check that chrono has basically the same iso8601/rfc3339 date
-    let Some(chrono_delta) = chrono::TimeDelta::new(t.seconds, t.nanos as u32) else {
+    assert!(DATE_RE.is_match(original_text));
+
+    // roundtrip to and from string
+    let roundtrip_text = format!("{timestamp}");
+    assert_eq!(Ok(&timestamp), roundtrip_text.parse().as_ref());
+
+    // chrono can only parse year 0000 till 9999
+    let Ok(chrono_time) = chrono::DateTime::parse_from_rfc3339(original_text) else {
         return;
     };
-    let Some(chrono_time) =
-        chrono::DateTime::<chrono::Utc>::UNIX_EPOCH.checked_add_signed(chrono_delta)
-    else {
+    if chrono_time.timestamp_subsec_nanos() > 999_999_999 {
+        // bilrost_types ignores leap seconds, but chrono increases the nanos in that case
         return;
-    };
-    let s3 = chrono_time.to_rfc3339();
-    assert_eq!(s2.strip_suffix("Z").unwrap(), s3.strip_suffix("+00:00").unwrap());
-    assert_eq!(Ok(&t), s3.parse().as_ref());
+    }
+
+    assert_eq!(timestamp.seconds, chrono_time.timestamp());
+    assert_eq!(timestamp.nanos, chrono_time.timestamp_subsec_nanos() as i32);
+
+    // roundtrip using chrono
+    let chrono_text = chrono_time.to_utc().to_rfc3339();
+    assert_eq!(
+        roundtrip_text.strip_suffix("Z"),
+        chrono_text.strip_suffix("+00:00")
+    );
 }
 
 enum RoundtripResult {
