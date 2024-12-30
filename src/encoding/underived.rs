a1640355d9e66f9b7237906ef442eef6a1f92ac9
Kent Ross <k@mad.cash>
2024-12-29 17:17:56 -0800

    add some doc comments and remove an early return Ok in underive_decode_distinguished

--- src/encoding/underived.rs
+++ src/encoding/underived.rs
@@ -1,3 +1,9 @@
+//! These macros provide a (fairly low-quality) shell to emulate derived message types without doing
+//! absolutely everything by hand, without an impossible dependency on the derive crate, and without
+//! being forced to use tuple types which might not be desirable. The implementations here are based
+//! on the code in the tuple encoder macro; these are mostly useful for making proxies.
+
+/// Fields must be listed in forward order here, and the targets should be &const.
 #[allow(unused_macros)]
 macro_rules! underived_encode {
     (
@@ -20,6 +26,7 @@ macro_rules! underived_encode {
 #[allow(unused_imports)]
 pub(crate) use underived_encode;
 
+/// Fields must be listed in reverse order here, and the targets should be &const.
 #[allow(unused_macros)]
 macro_rules! underived_prepend {
     (
@@ -42,6 +49,7 @@ macro_rules! underived_prepend {
 #[allow(unused_imports)]
 pub(crate) use underived_prepend;
 
+/// Fields must be listed in forward order here, and the targets should be &const.
 #[allow(unused_macros)]
 macro_rules! underived_encoded_len {
     (
@@ -60,6 +68,7 @@ macro_rules! underived_encoded_len {
 #[allow(unused_imports)]
 pub(crate) use underived_encoded_len;
 
+/// Fields can technically be listed in any order here; targets must be &mut.
 #[allow(unused_macros)]
 macro_rules! underived_decode {
     (
@@ -104,6 +113,7 @@ macro_rules! underived_decode {
 #[allow(unused_imports)]
 pub(crate) use underived_decode;
 
+/// Fields can technically be listed in any order here; targets must be &mut.
 #[allow(unused_macros)]
 macro_rules! underived_decode_distinguished {
     (
@@ -118,37 +128,38 @@ macro_rules! underived_decode_distinguished {
             let mut buf = $buf.take_length_delimited()?;
             let ctx = $ctx;
             if !ALLOW_EMPTY && buf.remaining_before_cap() == 0 {
-                return Ok(Canonicity::NotCanonical);
-            }
-            ctx.limit_reached()?;
-            let mut canon = Canonicity::Canonical;
-            let ctx = ctx.enter_recursion();
-            let tr = &mut TagReader::new();
-            let mut last_tag = None::<u32>;
-            while buf.has_remaining()? {
-                let (tag, wire_type) = tr.decode_key(buf.lend())?;
-                let duplicated = last_tag == Some(tag);
-                last_tag = Some(tag);
-                match tag {
-                    $($tag => {
-                        canon.update(DistinguishedEncoder::<$encoder>::decode_distinguished(
-                            wire_type,
-                            duplicated,
-                            $target,
-                            buf.lend(),
-                            ctx.clone(),
-                        ).map_err(|mut error| {
-                            error.push(stringify!($name), stringify!($field_name));
-                            error
-                        })?);
-                    })*
-                    _ => {
-                        skip_field(wire_type, buf.lend())?;
-                        canon.update(Canonicity::HasExtensions);
-                    },
+                Result::<_, crate::DecodeError>::Ok(Canonicity::NotCanonical)
+            } else {
+                ctx.limit_reached()?;
+                let mut canon = Canonicity::Canonical;
+                let ctx = ctx.enter_recursion();
+                let tr = &mut TagReader::new();
+                let mut last_tag = None::<u32>;
+                while buf.has_remaining()? {
+                    let (tag, wire_type) = tr.decode_key(buf.lend())?;
+                    let duplicated = last_tag == Some(tag);
+                    last_tag = Some(tag);
+                    match tag {
+                        $($tag => {
+                            canon.update(DistinguishedEncoder::<$encoder>::decode_distinguished(
+                                wire_type,
+                                duplicated,
+                                $target,
+                                buf.lend(),
+                                ctx.clone(),
+                            ).map_err(|mut error| {
+                                error.push(stringify!($name), stringify!($field_name));
+                                error
+                            })?);
+                        })*
+                        _ => {
+                            skip_field(wire_type, buf.lend())?;
+                            canon.update(Canonicity::HasExtensions);
+                        },
+                    }
                 }
+                Result::<_, crate::DecodeError>::Ok(canon)
             }
-            Ok(canon)
         }
     };
 }
